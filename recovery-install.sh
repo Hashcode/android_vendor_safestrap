0a3760f0c1fb0fbb513a3a61a49d3ffee18c1d16
hashcode <hashcode0f@gmail.com>
2011-09-13 00:10:10 -0700

    fixed install script

--- recovery-install.sh
+++ recovery-install.sh
@@ -2,24 +2,24 @@
 
 busybox_loc=`busybox which busybox`
 if [ "$busybox_loc" != "" ]; then
-	if [ -d CWRecovery ]; then
-		$busybox_loc rm -r CWRecovery
+	if [ -d install-files ]; then
+		$busybox_loc rm -r install-files
 	fi
-	$busybox_loc tar -zxf CWRecovery.tar.gz
+	$busybox_loc tar -zxf install-files.tar.gz
 	$busybox_loc mount -o remount,rw /system
 	if [ ! -f "/system/bin/logwrapper.orig" ]; then
 		$busybox_loc cp /system/bin/logwrapper /system/bin/logwrapper.orig
 	fi
-	cp CWRecovery/system/bin/logwrapper /system/bin
+	cp install-files/system/bin/logwrapper /system/bin
 	chown root.shell /system/bin/logwrapper
 	chmod 755 /system/bin/logwrapper
-	cp CWRecovery/system/bin/2nd-init /system/bin
+	cp install-files/system/bin/2nd-init /system/bin
 	chown root.shell /system/bin/2nd-init
 	chmod 755 /system/bin/2nd-init
 	if [ -f "/system/xbin/taskset" ]; then
 		rm /system/xbin/taskset
 	fi
-	cp CWRecovery/system/xbin/taskset /system/xbin
+	cp install-files/system/xbin/taskset /system/xbin
 	chown root.shell /system/xbin/taskset
 	chmod 755 /system/xbin/taskset
 	if [ -f "/system/xbin/cp" ]; then
@@ -31,7 +31,7 @@ if [ "$busybox_loc" != "" ]; then
 	fi
 	ln -s $busybox_loc /system/xbin/mount
 	if [ ! -d "/system/etc/rootfs" ]; then
-		cp -r CWRecovery/system/etc /system/etc
+		cp -r install-files/system/etc /system/etc
 		chown -R root.shell /system/etc/rootfs
 		chmod -R 644 /system/etc/rootfs
 	fi
@@ -41,8 +41,8 @@ if [ "$busybox_loc" != "" ]; then
 	if [ -d "/preinstall/recovery" ]; then
 		rm -r /preinstall/recovery
 	fi
-	cp -r CWRecovery/preinstall/recovery /preinstall
+	cp -r install-files/preinstall/recovery /preinstall
 	$busybox_loc mount -o ro,remount /preinstall
 else
-	echo 'Busybox required for this installation.  Please copy CWRecovery/system/bin/busybox to your PATH.  Installation aborted.'
+	echo 'Busybox required for this installation.  Please copy install-files/system/bin/busybox to your PATH.  Installation aborted.'
 fi
